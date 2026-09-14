c7ff88003d81f4c0cef51bb6b0fdd2d43ee01048
gregorbuerger <gregor.buerger@googlemail.com>
2026-09-14 13:03:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='platzspiel-v0.3';
-const ASSETS=['./','./index.html','./style.css?v=0.3','./app.js?v=0.3','./manifest.webmanifest?v=0.3'];
+const CACHE='platzspiel-v0.3.1';
+const ASSETS=['./','./index.html','./style.css?v=0.3.1','./app.js?v=0.3.1','./manifest.webmanifest?v=0.3.1'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
